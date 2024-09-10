8759159deb0b7e1541e4ab2c8cd9446144131f39
kalpesh gedia <kalpeshgedia@gmail.com>
2024-09-10 20:39:58 +0800

    initial commit

---
+++ src/components/Navbar/NavLinks.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { HashLink } from 'react-router-hash-link';
+
+const NavLinks = () => {
+    return (
+        <>
+            <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" smooth to="/">
+                Home
+            </HashLink>
+            <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/contact">
+                Contact Us
+            </HashLink>
+        </>
+    )
+}
+
+export default NavLinks;
